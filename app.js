a0db8f859d579238fbad22a5ca55c3efe58a353a
Sean Yeh <sean.yeh@gmx.com>
2017-01-07 11:49:59 -0600

    Many changes

--- app.js
+++ app.js
@@ -1,46 +1,122 @@
 var QUERIES = [
     "will be served",
-    "free+dinner",
     "pizza",
     "refreshments",
     "snacks",
     "cookies",
     "reception",
     "lunch will be",
+    "dinner will be",
+    "food will be",
+    "drinks will be",
+    "food until",
+    "free food",
+    "food provided",
 ];
 
+var MONTHS = [
+    "Jan", "Feb", "Mar", "Apr",
+    "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec"
+];
+
+
+/*
+ * Create DOM elements from an array of strings, and add them to a parent
+ * element
+ */
+function _mapDOM(arr, parentElement, newElement) {
+    var text;
+    for (var i = 0; i < arr.length; i++) {
+        text = arr[i];
+        parentElement.append($(newElement).text(text));
+    }
+}
+
+function toHumanTime(date) {
+    var ampm = "AM";
+
+    var m = (date.getMinutes() < 10? "0":"") + date.getMinutes();
+
+    // Convert hours to human readable
+    var h = date.getHours();
+    if (h > 12){
+        h -= 12;
+        ampm = "PM";
+    }
+    else if (h === 12){
+        ampm = "PM";
+    }
+    else if (h === 0){
+        h = "12";
+    }
+
+    return "" + h + ":" + m + " " + ampm;
+}
+
+
 function addItem(item) {
-    var itemDiv = $("<div class='block-group'>");
+    var itemDiv = $("<div id='" + item.id + "' class='block-group item'>");
     var dateDiv = $("<div class='block date'>");
     var descDiv = $("<div class='block desc'>");
 
+    // -- Date and Time --
     var itemDate = new Date(item.datetime.start);
 
-    var month = itemDate.toLocaleString("en-us", {"month":"short"});
+    // If event is in past, do not add
+    if (new Date() > new Date(item.datetime.end)) {
+        return;
+    }
+
+    var month = MONTHS[itemDate.getMonth()];
     var num = itemDate.getDate();
+
     dateDiv.append($("<div class='date-month'>").text(month));
     dateDiv.append($("<div class='date-num'>").text(num));
+    dateDiv.append($("<div class='date-time'>").text(toHumanTime(itemDate)));
 
 
-    // Title and Description
-    descDiv.append($("<h2>").text(item.title));
+    // -- Title and Description --
+    var link = $("<a>").attr("href", item.url).text(item.title);
+    descDiv.append($("<h2>").append(link));
 
+    // Info for location
+    var locationDiv = $("<div class='location'>");
 
-    var tempDesc;
-    for (var i = 0; i < item.description.length; i++) {
-        tempDesc = item.description[i];
-        descDiv.append($("<p>").text(tempDesc));
+    if (item.location) {
+        _mapDOM(item.location.address, locationDiv, "<div>");
     }
+    descDiv.append(locationDiv);
+
+    // Show/hide description toggle
+    var toggleLink = $("<a class='desc-toggle'>").text("Show description").attr("href", "#").click(function(e){
+        var state = $(this).text();
+        var div = $("#" + item.id).find(" .desc-text");
+        if (state === "Show description") {
+            div.show();
+            $(this).text("Hide description");
+        }
+        else {
+            div.hide();
+            $(this).text("Show description");
+        }
+
+        return false;
+    });
+    descDiv.append($("<div class='div-toggle'>").append(toggleLink));
+
+    var descTextDiv = $("<div class='desc-text'>");
+    _mapDOM(item.description, descTextDiv, "<p>");
+    descDiv.append(descTextDiv);
 
     itemDiv.append(dateDiv);
     itemDiv.append(descDiv);
 
     $("#main").append(itemDiv);
-
 }
 
 window.onload = function() {
-    var url = "http://api.nuevents.seanyeh.com/search?q=" + QUERIES.join("&q=");
+    var url = "http://api.nuevents.seanyeh.com/search?q='" + QUERIES.join("'&q='") + "'";
     $.getJSON(url, function(data){
         console.log("Received data!");
         console.log(data);
